894b5ae3ac0e042f839e9330c5f78864d444aacb
Bastien Dufour <77833441+bastid3v@users.noreply.github.com>
2024-11-15 09:28:39 +0100

    Test/group.test.by.rule (#1)

--- src/test/clean-asserter.spec.ts
+++ src/test/clean-asserter.spec.ts
@@ -1,67 +1,12 @@
 import { describe, expect, test } from 'bun:test';
+
 import { type Report, asserterHandler } from '@dragee-io/type/asserter';
-import type { Dragee } from '@dragee-io/type/common';
 import cleanAsserter from '../..';
-
-interface TestObject {
-    dragees: Dragee[];
-    result: {
-        pass: boolean;
-        errors: string[];
-    };
-}
-
-function rulePassed(drageeDirectory: string) {
-    test('Rule passed', () => {
-        const data: TestObject = require(drageeDirectory);
-        const report = asserterHandler(cleanAsserter, data.dragees);
-        expect(report.pass).toBe(data.result.pass);
-    });
-}
-
-function ruleFailed(drageeDirectory: string) {
-    test('Rule failed', () => {
-        const data: TestObject = require(drageeDirectory);
-        const report = asserterHandler(cleanAsserter, data.dragees);
-
-        expect(report.pass).toBe(data.result.pass);
-        for (const error of data.result.errors) {
-            expect(JSON.stringify(report.errors)).toContain(JSON.stringify(error));
-        }
-    });
-}
 
 describe('Clean Asserter', () => {
     test('assert with no dragees', () => {
         const report: Report = asserterHandler(cleanAsserter, []);
         expect(report.pass).toBeTrue();
         expect(report.namespace).toBe('clean');
-    });
-
-    describe('Use Case Rules', () => {
-        const USE_CASE_ALLOWED_DEPENDENCIES_TEST_DIRECTORY =
-            './clean/use-case-allowed-dependencies/';
-        describe('A use case must not have any dependency of type Controller/Presenter', () => {
-            rulePassed(`${USE_CASE_ALLOWED_DEPENDENCIES_TEST_DIRECTORY}/rule-passed.json`);
-            ruleFailed(`${USE_CASE_ALLOWED_DEPENDENCIES_TEST_DIRECTORY}/rule-failed.json`);
-        });
-    });
-
-    describe('Controller Rules', () => {
-        const CONTROLLER_MANDATORY_DEPENDENCIES_TEST_DIRECTORY =
-            './clean/controller-mandatory-dependencies/';
-        describe('A controller must at least contain one use case', () => {
-            rulePassed(`${CONTROLLER_MANDATORY_DEPENDENCIES_TEST_DIRECTORY}/rule-passed.json`);
-            ruleFailed(`${CONTROLLER_MANDATORY_DEPENDENCIES_TEST_DIRECTORY}/rule-failed.json`);
-        });
-    });
-
-    describe('Presenter Rules', () => {
-        const PRESENTER_MANDATORY_DEPENDENCIES_TEST_DIRECTORY =
-            './clean/presenter-mandatory-dependencies/';
-        describe('A presenter must at least contain one use case', () => {
-            rulePassed(`${PRESENTER_MANDATORY_DEPENDENCIES_TEST_DIRECTORY}/rule-passed.json`);
-            ruleFailed(`${PRESENTER_MANDATORY_DEPENDENCIES_TEST_DIRECTORY}/rule-failed.json`);
-        });
     });
 });
